7c47ed040768098c084b299348a67cad8957b25c
dlawhd <145415884+dlawhd@users.noreply.github.com>
2024-08-03 13:32:55 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/model/Music.java
@@ -0,0 +1,24 @@
+package com.example.musicupload.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "musics" +
+        "")
+public class Music {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String artist;
+    private String filePath;
+    private Timestamp uploadTime;
+
+    // 이 클래스는 데이터베이스 테이블 'music'과 매핑되는 엔터티...
+    // JPA가 자동으로 테이블을 생성 및 관리...
+}
