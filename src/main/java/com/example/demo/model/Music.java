11e3a12b3e6a1a767abfb499a6b1f3ac7693e820
dlawhd <145415884+dlawhd@users.noreply.github.com>
2024-08-05 13:14:41 +0900

    Update Music.java

--- src/main/java/com/example/demo/model/Music.java
+++ src/main/java/com/example/demo/model/Music.java
@@ -7,8 +7,7 @@ import java.sql.Timestamp;
 
 @Data
 @Entity
-@Table(name = "musics" +
-        "")
+@Table(name = "musics")
 public class Music {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +18,6 @@ public class Music {
     private String filePath;
     private Timestamp uploadTime;
 
-    // 이 클래스는 데이터베이스 테이블 'music'과 매핑되는 엔터티...
+    // 이 클래스는 데이터베이스 테이블 'musics'와 매핑되는 엔터티...
     // JPA가 자동으로 테이블을 생성 및 관리...
 }
