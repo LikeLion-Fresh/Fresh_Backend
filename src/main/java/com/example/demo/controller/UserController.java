8977292d5e48c5fcbb6be92280689c35c451cb5e
choiht0904 <choiht0904@naver.com>
2024-08-06 21:05:31 +0900

    add login check

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,15 @@ public class UserController {
             return ResponseEntity.ok("구독 취소 완료");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("구독 취소 중 오류 발생: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/login/check")
+    public ResponseEntity<Boolean> checkLogin(Principal principal) {
+        if(principal == null) {
+            return ResponseEntity.ok(Boolean.FALSE);
+        } else {
+            return ResponseEntity.ok(Boolean.TRUE);
         }
     }
 }
